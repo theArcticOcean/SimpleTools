15f91a34b97473a7a013ad65562294863c5d699d
WeiYang <wei_yang520@hotmail.com>
2021-01-30 19:50:36 +0800

    Update number.gd

--- godotGame/NumberGameIn3D/number.gd
+++ godotGame/NumberGameIn3D/number.gd
@@ -2,7 +2,12 @@ extends StaticBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func destroied():
 	queue_free()
+
+func InitNumber( num ):
+	var path : String = "res://tank_trexhwm_obj/number/"
+	path = path + num.to_string() + ".obj"
+	get_node("target").mesh = load( path )

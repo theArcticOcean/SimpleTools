c72fedb4a8ebd458592640e22dbbce22f3ce3d00
WeiYang <wei_yang520@hotmail.com>
2021-02-07 10:27:52 +0800

    Add GameOver Effect

--- godotGame/NumberGameIn3D/hub.gd
+++ godotGame/NumberGameIn3D/hub.gd
@@ -5,6 +5,7 @@ var operator: String
 var content: String
 onready var numGenerator: RandomNumberGenerator = RandomNumberGenerator.new()
 onready var restSecCount = get_node( "checkPointTimer" ).get_wait_time()
+export var g_PlayerAlive: bool = true
 
 func ShowGameOverUI( gameOver ):
 	if gameOver:
@@ -12,12 +13,22 @@ func ShowGameOverUI( gameOver ):
 		$restartButton.show()
 		get_tree().root.get_node("World/Environment").set_visible( false )
 		get_tree().root.get_node("World/player").set_visible( false )
+		get_tree().root.get_node("World/musics/BGM").stop()
+		get_tree().root.get_node("World/musics/failBGM").play()
+		g_PlayerAlive = false
+		$checkPointTimer.stop()
 	else:
 		$gameOverLabel.hide()
 		$restartButton.hide()
 		get_tree().root.get_node("World/Environment").set_visible( true )
 		get_tree().root.get_node("World/player").set_visible( true )
 		$resultLabel.text = ""
+		get_tree().root.get_node("World/musics/BGM").play()
+		get_tree().root.get_node("World/musics/failBGM").stop()
+		g_PlayerAlive = true
+		generateContent()
+		updateLabel()
+		$checkPointTimer.start()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -76,12 +87,11 @@ func _on_checkPointTimer_timeout():
 		updateLabel()
 		$resultLabel.text = ""
 	else:
-		ShowGameOverUI( true )
+		if g_PlayerAlive:
+			ShowGameOverUI( true )
 
 func _on_clear_result():
 	$resultLabel.text = ""
 
 func _on_restartButton_button_up():
 	ShowGameOverUI( false )
-	generateContent()
-	updateLabel()

7d4e39d207f4db68775a593b5530eac1c84b2a9d
theArcticOcean <wei_yang1994@163.com>
2021-03-06 21:58:05 +0800

    compress vtp file

--- 3DFilesConverter/vtpIO.cpp
+++ 3DFilesConverter/vtpIO.cpp
@@ -5,6 +5,7 @@
 #include <vtkXMLPolyDataReader.h>
 #include <vtkXMLPolyDataWriter.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 vtpIO::vtpIO()
 {
@@ -29,16 +30,31 @@ bool vtpIO::Read(std::string filePath)
 
 std::string vtpIO::Write(vtkSmartPointer<vtkPolyData> data, std::string filePath)
 {
-    if ( access( filePath.c_str(), F_OK ) == 0 )
+    auto baseName = filesManager::GetInstance()->GetBaseName( filePath );
+    if ( access( baseName.c_str(), F_OK ) == 0 )
+    {
+        Log( IInfo, "start to remove dir" );
+        filesManager::GetInstance()->RemoveDir( baseName.c_str() );
+    }
+    if ( mkdir( baseName.c_str(), 0777) )
+    {
+        Log( IError, strerror(errno) );
+        return INVALID_FILE;
+    }
+    auto newFilePath = baseName + "/result.vtp";
+
+    if ( access( newFilePath.c_str(), F_OK ) == 0 )
     {
         Log( IInfo, "start to remove exist file" );
-        filesManager::GetInstance()->RemoveDir( filePath.c_str() );
+        filesManager::GetInstance()->RemoveDir( newFilePath.c_str() );
     }
 
     vtkSmartPointer<vtkXMLPolyDataWriter> writer = vtkSmartPointer<vtkXMLPolyDataWriter>::New();
     writer->SetInputData( data );
-    writer->SetFileName( filePath.c_str() );
+    writer->SetFileName( newFilePath.c_str() );
     writer->Write();
 
-    return filePath;
+    auto zipFilePath = filesManager::GetInstance()->zip( newFilePath );
+
+    return zipFilePath;
 }
